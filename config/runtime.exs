d9b6a2ec990ff30a3471be0ea8e0dd7977585720
bonho <junhyeonsin@gmail.com>
2025-06-04 13:01:04 +0900

    add connection_pool

--- config/runtime.exs
+++ config/runtime.exs
@@ -6,6 +6,7 @@ config :restapi, MyApp.Repo,
   username: System.get_env("POSTGRES_USER", "postgres"),
   password: System.get_env("POSTGRES_PASSWORD", "postgres"),
   hostname: System.get_env("POSTGRES_SVC_SERVICE_HOST", "localhost"),
-  port: System.get_env("POSTGRES_SVC_SERVICE_PORT", "5432")
+  port: System.get_env("POSTGRES_SVC_SERVICE_PORT", "5432"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE", "10"))
 
 MyApp.ReleaseTask.migrate()
