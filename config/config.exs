fca61d8a10fb763fc8b3037d12249819204c77e5
bonho <junhyeonsin@gmail.com>
2025-06-04 13:25:08 +0900

    hash verifying was 2 sec

--- config/config.exs
+++ config/config.exs
@@ -1,2 +1,7 @@
 import Config
-config :restapi, ecto_repos: [MyApp.Repo]
+
+config :restapi,
+  ecto_repos: [MyApp.Repo]
+
+config :pbkdf2_elixir,
+  rounds: System.get_env("PBKDF2_ROUNDS", "80_000")
